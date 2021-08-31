82afd92a7e1e83fa2794cf00958210ca765ba9d7
shohely1710 <66716089+shohely1710@users.noreply.github.com>
2021-08-31 21:15:40 +0600

    Add Lists on menu and Display Tasks by List done!

--- list-task.php
+++ list-task.php
@@ -1,8 +1,9 @@
+
 <?php
-    include('config/constants.php');
-
+     include('config/constants.php');
+     //Get the listid from URL
+     $list_id_url = $_GET['list_id'];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +15,8 @@
 <body>
     <h1>TASK MANAGER</h1>
 
-    <!-- Menu Starts Here -->
-    <div class="menu">
+     <!-- Menu Starts Here -->
+     <div class="menu">
         <a href="<?php echo SITEURL; ?>">Home</a>
 
         <?php
@@ -52,74 +53,42 @@
     </div>
      <!-- Menu Ends Here -->
 
-     <!-- Tasks Starts Here -->
-     <p>
-         <?php
-            if(isset($_SESSION['add']))
-            {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']);
-            }
+     <div class="all-task">
+         <a href="<?php echo SITEURL; ?>add-task.php">Add Task</a>
 
-            if(isset($_SESSION['delete']))
-            {
-                echo $_SESSION['delete'];
-                unset($_SESSION['delete']);
-            }
-
-            if(isset($_SESSION['update']))
-            {
-                echo $_SESSION['update'];
-                unset($_SESSION['update']);
-            }
-
-            if(isset($_SESSION['delete_fail']))
-            {
-                echo $_SESSION['delete_fail'];
-                unset($_SESSION['delete_fail']);
-            }
-
-         ?>
-     </p>
-     <div class="all-tasks">
-         <a href="<?php SITEURL; ?>add-task.php">Add Task</a>
          <table>
              <tr>
-                 <th>S.N.</th>
+                 <th>S.N. </th>
                  <th>Task Name</th>
-                 <th>Priority</th>
-                 <th>Deadline</th>
-                 <th>Actions</th>
+                 <th>Priority </th>
+                 <th>Deadline </th>
+                 <th>Actions </th>
              </tr>
 
              <?php
-                //Connect Database
-                $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
 
-                //Select the Database
+                $conn = mysqli_connect(LOCALHOST,  DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
+
                 $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
 
-                //Create SQL Query to Get Data From Database
-                $sql = "SELECT * FROM tbl_tasks";
+                //SQL Query to display tasks by list selected
+                $sql = "SELECT * FROM tbl_tasks WHERE list_id=$list_id_url";
 
-                //Execute the Query
+                //Execute Query
                 $res = mysqli_query($conn, $sql);
 
-                //CHeck whether the query is executed or not
                 if($res==true)
                 {
-                    //Display the Tasks from Database
-
-                    //Count the Tasks on Database first
+                    //Display the tasks based on list
+                    //count the Rows
                     $count_rows = mysqli_num_rows($res);
 
-                    //Create Serial Number Variable
-                    $sn=1;
+                     //Create Serial Number Variable
+                     $sn=1;
 
-                    //CHeck whether there is task on database or not
                     if($count_rows>0)
                     {
-                        //Data is in Database
+                        //We have tasks on this list
                         while($row=mysqli_fetch_assoc($res))
                         {
                             $task_id = $row['task_id'];
@@ -127,37 +96,41 @@
                             $priority = $row['priority'];
                             $deadline = $row['deadline'];
                             ?>
-                             <tr>
-                                <td><?php echo $sn++; ?>. </td>
+
+                            <tr>
+                                <td><?php echo $sn++; ?>.</td>
                                 <td><?php echo $task_name; ?></td>
-                                <td><?php echo $priority; ?> </td>
+                                <td><?php echo $priority; ?></td>
                                 <td><?php echo $deadline; ?></td>
                                 <td>
                                     <a href="<?php echo SITEURL; ?>update-task.php?task_id=<?php echo $task_id; ?>">Update</a>   
-                                    <a href="<?php echo SITEURL; ?>delete-task.php?task_id=<?php echo $task_id; ?>">Delete</a>  
+                                    <a href="<?php echo SITEURL; ?>delete-task.php?task_id=<?php echo $task_id; ?>">Delete</a> 
                                 </td>
                             </tr>
 
                             <?php
-                        }
 
+                            
+                        }
                     }
                     else
                     {
-                        //No Data in Database
+                        //No Tasks on this list
                         ?>
                         <tr>
-                            <td colspan="5">No Task Added Yet. </td>
+                            <td colspan="5">No Tasks added on this list. </td>
                         </tr>
                         <?php
                     }
                 }
-             ?>
-         </table>
 
+             ?>
+
+            
+
+         </table>
      </div>
 
-      <!-- Tasks Ends Here -->
     
 </body>
 </html>
